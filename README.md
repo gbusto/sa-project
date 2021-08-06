13d07e68512719f5f7329572f7b46712a808daf8
Gabriel Busto <gbusto.gator@gmail.com>
2021-08-06 11:46:48 -0400

    Payment processing code has been added

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Take home project
-This is a simple e-commerce application that a customer can use to purchase a book, but it's missing the payments functionality — your goal is to integrate Stripe to get this application running!
-
-## Candidate instructions
-You'll receive these in email.
+This is a simple e-commerce application that a customer can use to purchase a book. The payments functionality has been added along with some additional code to help improve the user experience.
 
 ## Application overview
 This demo is written in Python with the [Flask framework](https://flask.palletsprojects.com/). You'll need to retrieve a set of testmode API keys from the Stripe dashboard (you can create a free test account [here](https://dashboard.stripe.com/register)) to run this locally.
@@ -11,11 +8,25 @@ We're using the [Bootstrap](https://getbootstrap.com/docs/4.6/getting-started/in
 
 To simplify this project, we're also not using any database here, either. Instead `app.py` includes a simple case statement to read the GET params for `item`. 
 
+If you would like to isolate the dependencies for this app, create a virtual environment:
+
+```
+python3 -m venv <your_env_name>
+source <your_env_name>/bin/activate
+```
+
 To get started, clone the repository and run pip3 to install dependencies:
 
 ```
 git clone https://github.com/marko-stripe/sa-takehome-project-python && cd sa-takehome-project-python
 pip3 install -r requirements.txt
+```
+
+Before running the application, you'll need to set two environment variables:
+
+```
+export STRIPE_PUBLIC_KEY=<your publishable Stripe key>
+export STRIPE_SECRET_KEY=<your secret Stripe key>
 ```
 
 Then run the application locally:
@@ -25,3 +36,5 @@ flask run
 ```
 
 Navigate to [http://localhost:5000](http://localhost:5000) to view the index page.
+
+Once you're done working, you can exit the virtual environment by typing `deactivate`.
--- app.py
+++ app.py
@@ -3,10 +3,36 @@ import stripe
 
 from flask import Flask, request, render_template
 
+# Used for printing output in color and with some formatting options
+class color:
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
+
 app = Flask(__name__,
   static_url_path='',
   template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), "views"),
   static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), "public"))
+
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+stripe.api_key = STRIPE_SECRET_KEY
+
+# NOTE: The publishable and secret keys are read through environment variables
+# In the event that they were not specified, the application will exit and
+# some warning output will be printed to notify the user/system running the app
+if not STRIPE_PUBLIC_KEY or not STRIPE_SECRET_KEY:
+  print(color.BOLD + color.YELLOW + '\n[!] Error: no Stripe keys specified. Please specify your keys using the following commands:' + color.END)
+  print(color.BOLD + color.YELLOW + '\texport STRIPE_PUBLIC_KEY=<stripe publishable key>' + color.END)
+  print(color.BOLD + color.YELLOW + '\texport STRIPE_SECRET_KEY=<stripe secret key>' + color.END)
+  exit(1)
 
 # Home route
 @app.route('/', methods=['GET'])
@@ -16,11 +42,12 @@ def index():
 # Checkout route
 @app.route('/checkout', methods=['GET'])
 def checkout():
-  # Just hardcoding amounts here to avoid using a database
+  # Retrieve the item number from the "item" URL parameter
   item = request.args.get('item')
   title = None
   amount = None
   error = None
+  client_secret = None
 
   if item == '1':
     title = 'The Art of Doing Science and Engineering'
@@ -32,15 +59,62 @@ def checkout():
     title = 'Working in Public: The Making and Maintenance of Open Source'
     amount = 2800
   else:
-    # Included in layout view, feel free to assign error
-    error = 'No item selected'
+    # An invalid item ID was provided and the item was not found
+    error = 'Item not found!'
 
-  return render_template('checkout.html', title=title, amount=amount, error=error)
+  # If the amount was specified, that means the user specified a valid product
+  if amount:
+    # Create a PaymentIntent based on the amount for this product
+    intent = stripe.PaymentIntent.create(
+      amount=amount,
+      currency='usd',
+    )
+    # Get the client secret from the newly created PaymentIntent
+    client_secret = intent.get('client_secret')
+
+  # Render the "checkout" template
+  return render_template('checkout.html',
+    public_key=STRIPE_PUBLIC_KEY,
+    title=title,
+    amount=amount,
+    error=error,
+    client_secret=client_secret,
+  )
 
 # Success route
 @app.route('/success', methods=['GET'])
 def success():
-  return render_template('success.html')
+  error = None
+  # Make it an empty dict for now in case the provided ID doesn't match any
+  # of our PaymentIntent objects
+  payment_intent = {}
+
+  # Attempt to retrieve the PaymentIntent ID from the URL parameters
+  pi_id_arg = request.args.get('pi')
+
+  # Check to make sure the user provided this parameter
+  if pi_id_arg:
+    try:
+      # Attempt to retrieve this PaymentIntent
+      payment_intent = stripe.PaymentIntent.retrieve(pi_id_arg)
+    except stripe.error.InvalidRequestError as err:
+      # PaymentIntent was most likely not found if we hit this error case
+      error = str(err)
+  else:
+    # If not, notify them of the error
+    error = 'No PaymentIntent ID provided'
+
+  # If payment_intent was never populated with actual data, these "gets" will
+  # safely return None
+  payment_intent_id = payment_intent.get('id')
+  amount = payment_intent.get('amount_received')
+
+  # Render the "success" template
+  return render_template('success.html',
+    error=error,
+    amount=amount,
+    payment_intent_id=payment_intent_id
+  )
 
 
 if __name__ == '__main__':
