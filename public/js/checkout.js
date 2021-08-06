86ea6e641e6d5a8ad5d75776e6bcc72f4c55e74d
Gabriel Busto <gbusto.gator@gmail.com>
2021-08-06 12:26:21 -0400

    Wrapped JS code in function.
    https://stackoverflow.com/questions/2421911/what-is-the-purpose-of-wrapping-whole-javascript-files-in-anonymous-functions-li

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,132 +1,134 @@
-var stripe = Stripe(publicKey);
+(function() {
+  var stripe = Stripe(publicKey);
 
-var form = $("#payment-form");
+  var form = $("#payment-form");
 
-var emailInput = $("#email");
+  var emailInput = $("#email");
 
-var cardIsComplete = false;
+  var cardIsComplete = false;
 
-function showCardError(message) {
-  var errorDiv = $("#card-errors");
-  errorDiv.append('<p class="text-danger">' + message + '</p>');
-}
-
-function clearCardError(message) {
-  var errorDiv = $("#card-errors");
-  errorDiv.empty();
-}
-
-function disableButton() {
-  $("#pay-button").prop("disabled", true);
-}
-
-function enableButton() {
-  $("#pay-button").prop("disabled", false);
-}
-
-function resetPayButton() {
-  var button = $("#pay-button");
-  button.html("Retry payment");
-  button.prop("disabled", false);
-}
-
-function isEmailValid() {
-  var re = /\w+[@]\w+\.\w{2}/;
-  var text = $("#email").val();
-  return re.exec(text);
-}
-
-emailInput.on("input", function(e) {
-  updateAllowPayment();
-});
-
-form.on("submit", function(e) {
-  clearCardError();
-
-  if (!inputIsComplete()) {
-    return;
+  function showCardError(message) {
+    var errorDiv = $("#card-errors");
+    errorDiv.append('<p class="text-danger">' + message + '</p>');
   }
-  var payButton = $("#pay-button");
-  var email = $("#email").val();
-  e.preventDefault();
-  disableButton();
-  payButton.html('<div class="spinner-border text-light mr-2"><span class="sr-only">Loading...</span></div>Processing Payment...');
 
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-      billing_details: {
-        email: email
-      }
+  function clearCardError(message) {
+    var errorDiv = $("#card-errors");
+    errorDiv.empty();
+  }
+
+  function disableButton() {
+    $("#pay-button").prop("disabled", true);
+  }
+
+  function enableButton() {
+    $("#pay-button").prop("disabled", false);
+  }
+
+  function resetPayButton() {
+    var button = $("#pay-button");
+    button.html("Retry payment");
+    button.prop("disabled", false);
+  }
+
+  function isEmailValid() {
+    var re = /\w+[@]\w+\.\w{2}/;
+    var text = $("#email").val();
+    return re.exec(text);
+  }
+
+  emailInput.on("input", function(e) {
+    updateAllowPayment();
+  });
+
+  form.on("submit", function(e) {
+    clearCardError();
+
+    if (!inputIsComplete()) {
+      return;
     }
-  }).then(function(result) {
-    if (result.error) {
-      showCardError(result.error.message);
-      resetPayButton();
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        window.location.href = "/success?pi=" + result.paymentIntent.id;
+    var payButton = $("#pay-button");
+    var email = $("#email").val();
+    e.preventDefault();
+    disableButton();
+    payButton.html('<div class="spinner-border text-light mr-2"><span class="sr-only">Loading...</span></div>Processing Payment...');
+
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          email: email
+        }
+      }
+    }).then(function(result) {
+      if (result.error) {
+        showCardError(result.error.message);
+        resetPayButton();
+      } else {
+        // The payment has been processed!
+        if (result.paymentIntent.status === 'succeeded') {
+          window.location.href = "/success?pi=" + result.paymentIntent.id;
+        }
+      }
+    });
+  });
+
+  var elements = stripe.elements({
+    locale: "auto"
+  });
+
+  /**
+   * Card Element
+   */
+  var card = elements.create("card", {
+    style: {
+      base: {
+        color: "#32325D",
+        fontWeight: 500,
+        fontFamily: "Inter, Open Sans, Segoe UI, sans-serif",
+        fontSize: "16px",
+        fontSmoothing: "antialiased",
+
+        "::placeholder": {
+          color: "#CFD7DF"
+        }
+      },
+      invalid: {
+        color: "#E25950"
       }
     }
   });
-});
 
-var elements = stripe.elements({
-  locale: "auto"
-});
+  card.on("change", ({complete, error}) => {
+    let displayError = document.getElementById("card-errors");
+    if (error) {
+      displayError.textContent = error.message;
+    } else {
+      displayError.textContent = "";
+    }
 
-/**
- * Card Element
- */
-var card = elements.create("card", {
-  style: {
-    base: {
-      color: "#32325D",
-      fontWeight: 500,
-      fontFamily: "Inter, Open Sans, Segoe UI, sans-serif",
-      fontSize: "16px",
-      fontSmoothing: "antialiased",
+    if (complete) {
+      cardIsComplete = true;
+    }
+    else {
+      cardIsComplete = false;
+    }
 
-      "::placeholder": {
-        color: "#CFD7DF"
-      }
-    },
-    invalid: {
-      color: "#E25950"
+    updateAllowPayment();
+  });
+
+  function inputIsComplete() {
+    return cardIsComplete && emailInput.val().length > 0 && isEmailValid();
+  }
+
+  function updateAllowPayment() {
+    if (inputIsComplete()) {
+      enableButton();
+    }
+    else {
+      disableButton();
     }
   }
-});
 
-card.on("change", ({complete, error}) => {
-  let displayError = document.getElementById("card-errors");
-  if (error) {
-    displayError.textContent = error.message;
-  } else {
-    displayError.textContent = "";
-  }
-
-  if (complete) {
-    cardIsComplete = true;
-  }
-  else {
-    cardIsComplete = false;
-  }
-
-  updateAllowPayment();
-});
-
-function inputIsComplete() {
-  return cardIsComplete && emailInput.val().length > 0 && isEmailValid();
-}
-
-function updateAllowPayment() {
-  if (inputIsComplete()) {
-    enableButton();
-  }
-  else {
-    disableButton();
-  }
-}
-
-card.mount("#stripe-elements");
+  card.mount("#stripe-elements");
+})();
